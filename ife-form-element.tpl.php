f5b4006bf3b7319c4078028af6256d4dd372881d
Stijn De Meyere <stijn@tak.nascom.be>
2010-02-04 11:26:18 +0100

    creating HEAD for IFE

--- ife-form-element.tpl.php
+++ ife-form-element.tpl.php
@@ -1,8 +1,16 @@
 <?php 
+<<<<<<< HEAD
+=======
+// $Id$
+>>>>>>> creating HEAD for IFE
 ?>
 
 <?php print $field; ?>
 
 <?php if ($error_message): ?>
+<<<<<<< HEAD
   <div class="messages error inline"><?php print $error_message; ?></div>
+=======
+  <div class='inline messages error'><?php print $error_message; ?></div>
+>>>>>>> creating HEAD for IFE
 <?php endif; ?>
