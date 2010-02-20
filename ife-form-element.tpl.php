6387a5f7c40e9855d6733ed75423f333120f7405
Stijn De Meyere <stijn@tak.local>
2010-02-20 13:03:45 +0100

    Fix for invalid HTML

--- ife-form-element.tpl.php
+++ ife-form-element.tpl.php
@@ -9,8 +9,12 @@
 
 <?php if ($error_message): ?>
 <<<<<<< HEAD
+<<<<<<< HEAD
   <div class="messages error inline"><?php print $error_message; ?></div>
 =======
   <div class='inline messages error'><?php print $error_message; ?></div>
 >>>>>>> creating HEAD for IFE
+=======
+  <div class="messages error inline"><?php print $error_message; ?></div>
+>>>>>>> Fix for invalid HTML
 <?php endif; ?>
