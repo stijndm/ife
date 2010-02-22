cfe107387cc9d743f6f3172445d1c48f30d20dc9
Stijn De Meyere <stijn@sken.be>
2010-02-22 19:11:04 +0100

    Coder review fixes. Mostly removed trailing spaces.

--- ife-form-element.tpl.php
+++ ife-form-element.tpl.php
@@ -1,8 +1,27 @@
+<<<<<<< HEAD
 <?php 
 <<<<<<< HEAD
 =======
 // $Id$
 >>>>>>> creating HEAD for IFE
+=======
+<?php
+// $Id$
+
+/**
+ * @file
+ *
+ * Theme implementation to display a form element with it's error.
+ *
+ * Available variables:
+ * - $field: the themed form element.
+ * - $error_message: The error message(s). Multiple messages are themed as a list.
+ * - $error: Returns TRUE if the form element has an error.
+ * - $element: The raw element data (array).
+ *
+ * @see template_preprocess_ife_form_element()
+ */
+>>>>>>> Coder review fixes. Mostly removed trailing spaces.
 ?>
 
 <?php print $field; ?>
@@ -11,6 +30,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
   <div class="messages error inline"><?php print $error_message; ?></div>
+<<<<<<< HEAD
 =======
   <div class='inline messages error'><?php print $error_message; ?></div>
 >>>>>>> creating HEAD for IFE
@@ -18,3 +38,6 @@
   <div class="messages error inline"><?php print $error_message; ?></div>
 >>>>>>> Fix for invalid HTML
 <?php endif; ?>
+=======
+<?php endif;
+>>>>>>> Coder review fixes. Mostly removed trailing spaces.
