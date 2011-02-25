77567dce8122e1b87a884d6597bf25df0bbcbff4
Stijn De Meyere <stijn@sken.be>
2011-02-25 16:40:47 +0100

    Fixing a dirty merge with original github remote

--- ife-form-element.tpl.php
+++ ife-form-element.tpl.php
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<?php 
-<<<<<<< HEAD
-=======
-// $Id$
->>>>>>> creating HEAD for IFE
-=======
 <?php
-// $Id$
 
 /**
  * @file
@@ -21,28 +13,10 @@
  *
  * @see template_preprocess_ife_form_element()
  */
->>>>>>> Coder review fixes. Mostly removed trailing spaces.
 ?>
 
 <?php print $field; ?>
 
 <?php if ($error_message): ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <div class="messages error inline"><?php print $error_message; ?></div>
-<<<<<<< HEAD
-=======
-  <div class='inline messages error'><?php print $error_message; ?></div>
->>>>>>> creating HEAD for IFE
-=======
-  <div class="messages error inline"><?php print $error_message; ?></div>
->>>>>>> Fix for invalid HTML
-<?php endif; ?>
-=======
-<?php endif;
->>>>>>> Coder review fixes. Mostly removed trailing spaces.
-=======
   <div class="messages error messages-inline"><?php print $error_message; ?></div>
 <?php endif;
->>>>>>> Changed by swentel: Change default class names.
